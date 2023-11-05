f3b651233478bf139aba583b11b2f776e1d5d066
MekbibLovesTests <mekbibtare@gmail.com>
2023-11-05 15:31:48 +0300

    Catch errors if all promises are rejects

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("hello world")
 if (!Promise.first) {
   Promise.first = function(prs) {
-
+    var rejectCount = 0;
     return new Promise(function(resolve, reject) {
       // loop through all promises
       prs.forEach(function(pr) {
@@ -9,15 +9,19 @@ if (!Promise.first) {
         Promise.resolve(pr)
           // whichever one fulfills first wins, and
           // gets to resolve the main promise
-          .then(resolve)
+          .then(resolve, () => {
+            rejectCount += 1;
+            if (rejectCount === prs.length)
+              reject("new error")
+          })
           });
       })
  
   };
 }
 
-var p1 = Promise.resolve(1);
-var p2 = Promise.resolve(2);
+var p1 = Promise.reject(1);
+var p2 = Promise.reject(2);
 var p3 = Promise.resolve(3);
 
 Promise.first([p1,p2,p3]).then(
